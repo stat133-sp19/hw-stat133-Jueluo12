0c37d3348a23dec6cba1f37cd6a94b84fe17387b
jueluo12 <jueluo@berkeley.edu>
2019-04-17 19:43:38 -0700

    checkout

--- workout02/app.R
+++ workout02/app.R
@@ -13,24 +13,24 @@ ui <- fluidPage(
    fluidRow (
      column(3,
             sliderInput("initial", h5("Initial Amount"),
-                        min = 1, max = 100000, value = 1000, pre = "$")),
+                        min = 0, max = 100000, value = 1000, step = 500, pre = "$")),
      
      column(3,
             sliderInput("return_rate", h5("Return Rate(in %)"),
-                       min = 0, max = 20, value = 5)),
+                       min = 0, max = 20, step = 0.1, value = 5)),
      column(3,
             sliderInput("years", h5("Years"),
-                       min = 0, max =50, value = 10, pre = "$"))
+                       min = 0, max =50, value = 10, step = 1, pre = "$"))
    ),  
    
    fluidRow (
      column(3,
             sliderInput("annual", h5("Annual Contribution"),
-                        min = 0, max = 50000, value = 2000, pre = "$")),
+                        min = 0, max = 50000, value = 2000, step = 500, pre = "$")),
      
      column(3,
             sliderInput("growth_rate", h5("Growth Rate(in %)"),
-                        min = 0, max = 20, value = 2)),
+                        min = 0, max = 20, step = 0.1, value = 2)),
      column(3,
             selectInput("facet", h5("Facet?"),
                         choices = list("No" = FALSE, "Yes" = TRUE), selected = 1))
@@ -38,7 +38,7 @@ ui <- fluidPage(
    titlePanel("Timelines"),
    plotOutput("plot1"),
    titlePanel("Balances"),
-   dataTableOutput("table")
+   verbatimTextOutput("table")
 )
 
 
@@ -60,13 +60,13 @@ server <- function(input, output) {
       return(result)
     }
     growing_annuity <- function(contrib, rate, growth, years) {
-      result = contrib * ((1 + rate)^ years -(1 + growth)^years) / (rate - growth)
+      result = contrib * (((1 + rate)^ years - (1 + growth)^years) / (rate - growth))
       return(result)
     }
-    modalities <- matrix( , ncol = 4, nrow = 11)
+    modalities <- matrix( , ncol = 4, nrow = years+1)
     cnames <- c("year", "no_contrib", "fixed_contrib", "growing_contrib")
     colnames(modalities) <- cnames
-    for (i in 1:11) {
+    for (i in 1: (years+1)) {
       modalities[i,1] <- i-1
       modalities[i,2] <- future_value(amount, rate, i-1) 
       modalities[i,3] <- future_value(amount, rate, i-1) + annuity(contrib, rate , i-1)
@@ -81,18 +81,18 @@ server <- function(input, output) {
     p <- ggplot(plot1.data, aes(year, value, group = Modalities)) +
       geom_line(aes(color = Modalities)) +
       geom_point(aes(color = Modalities)) +
-      scale_x_continuous(name = "year", breaks = 0:10) +
+      scale_x_continuous(name = "year", breaks = 0: (nrow(dataInput())-1))+
       scale_y_continuous(name = "value") + 
       ggtitle("Three modes of investing") +
       theme_bw() 
     if(input$facet ==TRUE) {
-      p <- p + facet_grid( ~ Modalities) + geom_area(aes(fill = Modalities))
+      p <- p + facet_grid( ~ Modalities) + geom_area(aes(fill = Modalities), alpha = 0.6)
     }
     p
   })
   
-  output$table <- renderDataTable({
-    dataInput()
+  output$table <- renderPrint({
+    print(dataInput())
   })
 }
 
